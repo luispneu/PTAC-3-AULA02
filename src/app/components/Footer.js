e4e2dfc75fa4c127ccf8a93f38cc4d88828b406b
luispneu <luisgabriel8328@gmail.com>
2024-02-28 17:37:06 -0400

    css menu

---
+++ src/app/components/Footer.js
@@ -0,0 +1,10 @@
+
+export default function Footer(){
+    return(
+        <header>
+        <footer>
+        <h1>Footer legal</h1>
+        </footer>
+        </header>
+    );
+    }
---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+import Link from "next/link"
+export default function Notfound(){
+    return(
+        <div>
+        <h1>Error!</h1>
+        <link href="/">Retornar para Home</link>
+        </div>
+    )
+}
