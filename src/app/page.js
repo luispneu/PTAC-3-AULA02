6d762d06f54be8c6971d8eec04d10c0235b09161
luispneu <luisgabriel8328@gmail.com>
2024-03-13 17:34:53 -0400

    img, route

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
-export default async function Home(){
-  return(
-      <main>
-          <h1>zfsadgadsfhbdafhdzsfab</h1>
-      </main>
-  )
+export default async function Home() {
+  const respostas = await fetch("http://localhost:3000/api",{
+    next:{
+      revalidate: 1
+    }
+  });
+  const campus = await respostas.json();
+  return (
+    <main>
+      <h1>Home!!</h1>
+      {campus.map((campi) => (
+        <div>
+          <p>{campi.nome_campi}</p>
+      <img src="campi.imagem_url"></img>
+        </div>
+      ))}
+    </main>
+  );
 }
