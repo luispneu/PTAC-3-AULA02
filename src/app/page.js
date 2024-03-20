e0d7c461dadca0ae40e9abf0552ddea9f11675e6
hefraimjr <luisgabriel8328@gmail.com>
2024-03-20 17:27:29 -0400

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import Image from "next/Image";
+
 export default async function Home() {
   const respostas = await fetch("https://back-end-ifms-rho.vercel.app/campi",{
     next:{
@@ -10,8 +12,9 @@ export default async function Home() {
       <h1>Home!!</h1>
       {campus.map((campi) => (
         <div>
+      
           <p>{campi.nome_campus}</p>
-      <img src="campus.image_url"></img>
+      <Image   widht={100} height={100}  src="campus.image_url"/>
         </div>
       ))}
     </main>
